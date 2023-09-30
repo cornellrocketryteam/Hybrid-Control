a79d45828b613c18ec8a8f49eb8932d3e6119105
Cameron Goddard <camerongoddard@me.com>
2023-09-29 23:23:29 -0400

    restructure view into separate class

--- src/controller.py
+++ src/controller.py
@@ -3,55 +3,23 @@ controller.py: Manages user commands and actions
 """
 
 from test_stand import TestStand
-import view
+from view import TUI
 import curses
-
-stdscr = curses.initscr()
-curses.echo()
-#curses.start_color()
-stdscr.clear()
 
 class Controller:
 
     def __init__(self, handle: int) -> None:
         self.test_stand = TestStand(handle)
+        self.tui = TUI(self.test_stand)
         self.handle = handle
 
     def run(self) -> None:
-        input_str = "> "
         try:
             while True:
-                stdscr.refresh()
+                self.tui.update_screen()
+                c = self.tui.get_input()
 
-                # Display
-
-                stdscr.addstr(0, 0, "=" * curses.COLS)
-                stdscr.addstr(1, 0, "Hybrid Test Stand Control")
-                stdscr.addstr(2, 0, "=" * curses.COLS)
-
-                for i in range(0, len(self.test_stand.sv_states)):
-                    sv_str = "SV {num}: {state}".format(num = i+1, state = "ON" if self.test_stand.sv_states[i] else "OFF")
-                    stdscr.addstr(i+4, 0, sv_str)
-
-                mav_str_1 = "MAV 1: {state}".format(state = "ON" if self.test_stand.mav_states[0] else "OFF")
-                mav_str_2 = "MAV 2: {state}".format(state = "ON" if self.test_stand.mav_states[1] else "OFF")
-                stdscr.addstr(10, 0, mav_str_1)
-                stdscr.addstr(11, 0, mav_str_2)
-
-                stdscr.addstr(4, 50, "Default")
-
-                stdscr.addstr(6, 50, "Prefire purge tanks")
-                stdscr.addstr(7, 50, "Prefire purge engine")
-                stdscr.addstr(8, 50, "Fill")
-                stdscr.addstr(9, 50, "Supercharge")
-                stdscr.addstr(10, 50, "Ignition")
-                stdscr.addstr(11, 50, "Fire")
-
-                stdscr.addstr(curses.LINES - 1, 0, input_str)
-
-                # Process input
-
-                c = stdscr.getch()
+                input_str = self.tui.input_str
 
                 if c == 27:
                     break
@@ -76,17 +44,16 @@ class Controller:
                         else:
                             self.test_stand.mav_off(int(words[1]))
 
-                    input_str = "> "
-                    stdscr.clear()
+                    self.tui.input_str = "> "
+                    self.tui.clear()
 
                 elif c == curses.KEY_BACKSPACE or c == 127:
                     if len(input_str) > 2:
                         input_str = input_str[:-1]
-                        stdscr.clear()
+                        self.tui.clear()
                 else:
-                    input_str += chr(c)
+                    self.tui.input_str += chr(c)
 
         except KeyboardInterrupt:
-            # Teardown
             pass
         
--- src/view.py
+++ src/view.py
@@ -1,3 +1,52 @@
+"""
+view.py: Manages the text-based user interface
+"""
+
+import curses
 
 
+class TUI:
+    def __init__(self, test_stand) -> None:
+        self.stdscr = curses.initscr()
+        curses.echo()
+        #curses.start_color()
+        self.stdscr.clear()
 
+        self.input_str = "> "
+
+        self.test_stand = test_stand
+    
+    def update_screen(self) -> None:
+        self.stdscr.refresh()
+
+        # Can move to init?
+        self.stdscr.addstr(0, 0, "=" * curses.COLS)
+        self.stdscr.addstr(1, 0, "Hybrid Test Stand Control")
+        self.stdscr.addstr(2, 0, "=" * curses.COLS)
+
+        for i in range(0, len(self.test_stand.sv_states)):
+            sv_str = "SV {num}: {state}".format(num = i+1, state = "ON" if self.test_stand.sv_states[i] else "OFF")
+            self.stdscr.addstr(i+4, 0, sv_str)
+
+        mav_str_1 = "MAV 1: {state}".format(state = "ON" if self.test_stand.mav_states[0] else "OFF")
+        mav_str_2 = "MAV 2: {state}".format(state = "ON" if self.test_stand.mav_states[1] else "OFF")
+        self.stdscr.addstr(10, 0, mav_str_1)
+        self.stdscr.addstr(11, 0, mav_str_2)
+
+        self.stdscr.addstr(4, 50, "Default")
+
+        self.stdscr.addstr(6, 50, "Prefire purge tanks")
+        self.stdscr.addstr(7, 50, "Prefire purge engine")
+        self.stdscr.addstr(8, 50, "Fill")
+        self.stdscr.addstr(9, 50, "Supercharge")
+        self.stdscr.addstr(10, 50, "Ignition")
+        self.stdscr.addstr(11, 50, "Fire")
+
+        self.stdscr.addstr(curses.LINES - 1, 0, self.input_str)
+
+    def get_input(self) -> int:
+        c = self.stdscr.getch()
+        return c
+    
+    def clear(self) -> None:
+        self.stdscr.clear()
