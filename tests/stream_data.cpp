d8a8340a7156e3a46e0ef26156439b927c7079f2
Nissi Ragland <nkr33@cornell.edu>
2024-01-14 23:15:35 -0500

    add c++ tests folder and makefile

---
+++ tests/stream_data.cpp
@@ -0,0 +1,55 @@
+#include <LJM_Utilities.h>
+#include <LabJackM.h>
+#include <stdio.h>
+
+int main() {
+    int err;
+    int handle;
+    int i;
+
+ // Set up for configuring the AIN
+ // AIN0:
+ //   Negative channel = single ended (199)
+ //   Range: +/-10.0 V (10.0).
+ //   Resolution index = Default (0)
+ //   Settling, in microseconds = Auto (0)
+    enum { NUM_FRAMES_CONFIG = 4 };
+    const char *aNamesConfig[NUM_FRAMES_CONFIG] =
+        {"AIN0_NEGATIVE_CH", "AIN0_RANGE", "AIN0_RESOLUTION_INDEX", "AIN0_SETTLING_US"};
+    const double aValuesConfig[NUM_FRAMES_CONFIG] = {199, 10, 0, 0};
+    int errorAddress = INITIAL_ERR_ADDRESS;
+
+ // Set up for reading AIN value
+    double value = 0;
+    const char *NAME = "AIN0";
+
+ // Open first found LabJack
+    handle = OpenOrDie(LJM_dtANY, LJM_ctANY, "LJM_idANY");
+ // handle = OpenSOrDie("LJM_dtANY", "LJM_ctANY", "LJM_idANY");
+
+    PrintDeviceInfoFromHandle(handle);
+    printf("\n");
+
+ // Setup and call eWriteNames to configure AIN0 on the LabJack.
+    err = LJM_eWriteNames(handle, NUM_FRAMES_CONFIG, aNamesConfig, aValuesConfig,
+                          &errorAddress);
+    ErrorCheckWithAddress(err, errorAddress, "LJM_eWriteNames");
+
+    printf("Set configuration:\n");
+    for (i = 0; i < NUM_FRAMES_CONFIG; i++) {
+        printf("    %s : %f\n", aNamesConfig[i], aValuesConfig[i]);
+    }
+
+ // Read AIN0 from the LabJack
+    err = LJM_eReadName(handle, NAME, &value);
+    ErrorCheck(err, "LJM_eReadName");
+
+ // Print results
+    printf("\n%s : %f V\n", NAME, value);
+
+    CloseOrDie(handle);
+
+    WaitForUserIfWindows();
+
+    return LJME_NOERROR;
+}
